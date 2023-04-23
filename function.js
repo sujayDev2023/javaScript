e6da84f2a1728c4818e27d9e7dee4c725198838c
sujay <sujaym046@gmail.com>
2023-04-23 22:34:32 +0530

    add anather example

--- function.js
+++ function.js
@@ -3,4 +3,9 @@
 
   const multiplication = arr.map((element) => { return element*2 });
 
-  console.log(multiplication);
+  console.log(multiplication); 
+
+
+const mul = arr.map((newArray)=> newArray*4);
+
+console.log(mul);
